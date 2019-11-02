338618804521b73cc1c6b2b8d107ea4c2ef56927
zhengcheng01 <zhengcheng01@ecsemir.com>
2019-11-01 22:09:48 -0700

    twitter finished

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Cheng Zheng on 11/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
